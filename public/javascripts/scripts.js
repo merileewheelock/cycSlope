7ab593cc32a99806bdae99fa75a0ff4a35ee1808
Merilee Wheelock <eelirem@Merilees-MacBook-Air.local>
2017-06-07 16:42:30 -0400

    Adding second screen to homepage

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	
+	$('.go-button').click(function(){
+		$('.page1,.page2').css({
+			'top':'100vh'
+		});
+	});
+	$('.back-home').click(function(){
+		$('.page1,.page2').css({
+			'top':'0vh'
+		});
+	});
+
 });
