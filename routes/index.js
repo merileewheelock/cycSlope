fbc4a2f406c63c7c88280a062648b7dd24b5b4ee
Marissa Monivis <mmonivis@gmail.com>
2017-06-08 13:35:12 -0400

    Updated index.js style.css and register-modal.ejs

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,17 @@ var connection = mysql.createConnection({
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/register', function(req,res){
-    res.render('register')
+    var message = req.query.msg;
+    if (message == 'badEmailRegister'){
+        message = 'This email is already in use'
+    }
+  res.render('index', { 
+    title: 'Express',
+    message: message
+    });
 });
 router.post('/processRegister', function(req,res){
-	// console.log(req.session)
+    // console.log(req.session)
     var username = req.body.username
     var email = req.body.email
     var password = req.body.password
@@ -28,33 +32,41 @@ router.post('/processRegister', function(req,res){
     // console.log(password)
     var selectQuery = "SELECT * FROM userInfo WHERE email = ?";
     connection.query(selectQuery,[email], function(error, results){
-    	console.log('-------------')
-    	console.log(results);
+        console.log('-------------')
+        console.log(results);
         if(results.length == 0){
             var insertQuery = "INSERT INTO userInfo (username,email,password) VALUES (?,?,?)";
             connection.query(insertQuery,[username,email,password], function(error,results){
-       			// console.log("================");
-			    // console.log(req.session);
-			    // console.log("================");
+                // console.log("================");
+                // console.log(req.session);
+                // console.log("================");
                 req.session.name = username;
                 req.session.email = email;
                 req.session.loggedin = true;
                 res.redirect('/?msg=registered')
             });
         }else{
-            res.redirect('/signup?msg=badEmail')
+            res.redirect('/?msg=badEmailRegister')
         };
     });
 });
-
-router.get('/login', function(req,res){
-    res.render('login')
-});
-
 router.post('/processLogin', function(req,res){
     var email = req.body.email
     var password = req.body.password
     var selectQuery = "SELECT * FROM userInfo WHERE email = ?";
+    connection.query(selectQuery, [email], function(error,results){
+        if(results.length == 1){
+            if (password == results[0].password){
+                req.session.loggedin = true;
+                req.session.name = results.name;
+                req.session.email = results.email;
+                res.redirect('/?msg=loggedin')
+            }else{
+                res.redirect('/login?msg=badPass')
+            }
+        }else{
+            res.redirect('/login?msg=badEmail')
+        }
+    });
 });
-
 module.exports = router;
